2fb6a11046d56f2f8657b2be0d5c1334aafb8307
wodjafink <wodjafink@gmail.com>
2018-04-29 03:51:24 -0700

    Spotify package added, able to query and return a song name but it must be in quotes

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 var Twitter = require('twitter');
+var Spotify = require('node-spotify-api')
 var key = require('./key.js')
 
 var client = new Twitter({
@@ -8,6 +9,11 @@ var client = new Twitter({
   access_token_key: key.twitter.access_token_key,
   access_token_secret: key.twitter.access_token_secret
 });
+
+var spotify = new Spotify({
+	id: key.spotify.id,
+	secret: key.spotify.secret
+})
 
 var params = {screen_name: 'jmednick_tw'};
 
@@ -25,11 +31,48 @@ switch (doThis)
 		  } else {
 		  	console.log(error)
 		  }
-
 		});
 		break;
 	case "spotify-this-song":
-		console.log("Begin spotify-this-song")
+		if (process.argv[3]) {
+			// Search for the query defined by the user
+			spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
+	  			if (err) {
+	    			return console.log('Error occurred: ' + err);
+	  			}
+
+	  			data.tracks.items.forEach(function(item){
+	  				if (item.name === process.argv[3]){  					
+		  				console.log("Artists: ")
+		  				item.artists.forEach(function(artist){
+		  					console.log("\t * " + artist.name)
+		  				})
+		  				console.log("Track: " + item.name)
+		  				console.log("Preview: " + item.preview_url)
+		  				console.log("Album Name: " + item.album.name)
+	  				}
+	  			})
+			});
+		} else {
+			// Default is to return "The Sign" by Ace of Base
+			spotify.search({ type: 'track', query: 'The Sign' }, function(err, data) {
+	  			if (err) {
+	    			return console.log('Error occurred: ' + err);
+	  			}
+
+	  			data.tracks.items.forEach(function(item){
+	  				if (item.name === "The Sign"){  					
+		  				console.log("Artists: ")
+		  				item.artists.forEach(function(artist){
+		  					console.log("\t * " + artist.name)
+		  				})
+		  				console.log("Track: " + item.name)
+		  				console.log("Preview: " + item.preview_url)
+		  				console.log("Album Name: " + item.album.name)
+	  				}
+	  			})
+			});
+		}
 		break;
 	case "movie-this":
 		console.log("Begin movie-this")
