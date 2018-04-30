4a86eea94489ff4fd693d12a3c7a306868c04ef4
wodjafink <wodjafink@gmail.com>
2018-04-30 12:51:42 -0700

    Refactored code to put the calls into a function in the effort of making reading from the random.txt file easier

--- liri.js
+++ liri.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api')
 var request = require('request')
+var fs = require('fs')
 var key = require('./key.js')
 
 var client = new Twitter({
@@ -76,133 +77,151 @@ function movieEntry(name, formattedName){
 	}
 }
 
+function doTwitter(){
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  if (!error) {
+	    tweets.forEach(function(tweet){
+	    	console.log(tweet.text)
+	    })
+	  } else {
+	  	console.log(error)
+	  }
+	});
+}
+
+function doSpotify(){
+	if (process.argv[3]) {
+		// Search for the query defined by the user
+		spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
+  			if (err) {
+    			return console.log('Error occurred: ' + err);
+  			}
+  			var i = 0;
+  			data.tracks.items.some(function(item){
+  				if (item.name.toLowerCase() === process.argv[3].toLowerCase()){  					
+	  				i++;
+	  				console.log("Artists: ")
+	  				item.artists.forEach(function(artist){
+	  					console.log("\t * " + artist.name)
+	  				})
+	  				console.log("Track: " + item.name)
+	  				console.log("Preview: " + item.preview_url)
+	  				console.log("Album Name: " + item.album.name)
+  				}
+  				return i === 1;
+  			})
+		});
+	} else {
+		// Default is to return "The Sign" by Ace of Base
+		spotify.search({ type: 'track', query: 'The Sign' }, function(err, data) {
+  			if (err) {
+    			return console.log('Error occurred: ' + err);
+  			}
+  			var i = 0;
+  			data.tracks.items.some(function(item){
+  				if (item.name === "The Sign"){  					
+	  				i++;
+	  				console.log("Artists: ")
+	  				item.artists.forEach(function(artist){
+	  					console.log("\t * " + artist.name)
+	  				})
+	  				console.log("Track: " + item.name)
+	  				console.log("Preview: " + item.preview_url)
+	  				console.log("Album Name: " + item.album.name)
+  				}
+  				return i === 1;
+  			})
+		});
+	}
+}
+
+function doMovie(){
+	if (process.argv[3]){
+		var userMovieEntry = new movieEntry(process.argv[3], process.argv[3].replace(/ /g,"-"));
+		request('http://www.omdbapi.com/?apikey=trilogy&s=' + userMovieEntry.formattedName, function(err, response, body){
+  			if (err) {
+    			return console.log('Error occurred: ' + err);
+  			}
+  			var movies = JSON.parse(body);
+  			var i = 0;
+  			movies.Search.some(function(movie){
+  				i++;
+  				if (movie.Title === userMovieEntry.name){
+  					userMovieEntry.setYear(movie.Year);
+  					userMovieEntry.setID(movie.imdbID);
+					request('http://www.omdbapi.com/?apikey=trilogy&i=' + userMovieEntry.ID, function(err, response, body){
+						if (err) {
+			    			return console.log('Error occurred: ' + err);
+			  			}
+			  			var imdbMovie = JSON.parse(body);
+			  			userMovieEntry.setimdbRating(imdbMovie.imdbRating);
+			  			if (imdbMovie.Ratings[1] != undefined){
+				  			userMovieEntry.setRottenTomatoesRating(imdbMovie.Ratings[1].Value)
+			  			} else {
+			  				userMovieEntry.setRottenTomatoesRating(" No Rating Sorry! ")
+			  			}
+			  			userMovieEntry.setCountry(imdbMovie.Country);
+			  			userMovieEntry.setLanguage(imdbMovie.Language)
+			  			userMovieEntry.setPlot(imdbMovie.Plot)
+			  			userMovieEntry.setActors(imdbMovie.Actors)
+						userMovieEntry.displayMovie();
+					})
+					//Adding a return here to only print one movie
+  				}
+  				return i === 1;
+  			})
+		})
+	} else {
+		var defaultMovieEntry = new movieEntry('Mr. Nobody', 'Mr-Nobody');
+		request('http://www.omdbapi.com/?apikey=trilogy&s=' + defaultMovieEntry.formattedName, function(err, response, body){
+  			if (err) {
+    			return console.log('Error occurred: ' + err);
+  			}
+  			var movies = JSON.parse(body);
+  			movies.Search.forEach(function(movie){
+  				if ((movie.Title === "Mr. Nobody") && (movie.Year === '2009')){
+  					defaultMovieEntry.setYear(movie.Year);
+  					defaultMovieEntry.setID(movie.imdbID);
+					request('http://www.omdbapi.com/?apikey=trilogy&i=' + defaultMovieEntry.ID, function(err, response, body){
+						if (err) {
+			    			return console.log('Error occurred: ' + err);
+			  			}
+			  			var imdbMovie = JSON.parse(body);
+			  			defaultMovieEntry.setimdbRating(imdbMovie.imdbRating);
+			  			defaultMovieEntry.setRottenTomatoesRating(imdbMovie.Ratings[1].Value)
+			  			defaultMovieEntry.setCountry(imdbMovie.Country);
+			  			defaultMovieEntry.setLanguage(imdbMovie.Language)
+			  			defaultMovieEntry.setPlot(imdbMovie.Plot)
+			  			defaultMovieEntry.setActors(imdbMovie.Actors)
+						defaultMovieEntry.displayMovie();
+					})
+  				}
+  			})
+		})
+	}
+	
+}
+
 switch (doThis)
 {
 	case "my-tweets":
-		console.log("Begin my-tweets")
-		client.get('statuses/user_timeline', params, function(error, tweets, response) {
-		  if (!error) {
-		    tweets.forEach(function(tweet){
-		    	console.log(tweet.text)
-		    })
-		  } else {
-		  	console.log(error)
-		  }
-		});
+		doTwitter();
 		break;
 	case "spotify-this-song":
-		if (process.argv[3]) {
-			// Search for the query defined by the user
-			spotify.search({ type: 'track', query: process.argv[3] }, function(err, data) {
-	  			if (err) {
-	    			return console.log('Error occurred: ' + err);
-	  			}
-	  			var i = 0;
-	  			data.tracks.items.some(function(item){
-	  				if (item.name.toLowerCase() === process.argv[3].toLowerCase()){  					
-		  				i++;
-		  				console.log("Artists: ")
-		  				item.artists.forEach(function(artist){
-		  					console.log("\t * " + artist.name)
-		  				})
-		  				console.log("Track: " + item.name)
-		  				console.log("Preview: " + item.preview_url)
-		  				console.log("Album Name: " + item.album.name)
-	  				}
-	  				return i === 1;
-	  			})
-			});
-		} else {
-			// Default is to return "The Sign" by Ace of Base
-			spotify.search({ type: 'track', query: 'The Sign' }, function(err, data) {
-	  			if (err) {
-	    			return console.log('Error occurred: ' + err);
-	  			}
-	  			var i = 0;
-	  			data.tracks.items.some(function(item){
-	  				if (item.name === "The Sign"){  					
-		  				i++;
-		  				console.log("Artists: ")
-		  				item.artists.forEach(function(artist){
-		  					console.log("\t * " + artist.name)
-		  				})
-		  				console.log("Track: " + item.name)
-		  				console.log("Preview: " + item.preview_url)
-		  				console.log("Album Name: " + item.album.name)
-	  				}
-	  				return i === 1;
-	  			})
-			});
-		}
+		doSpotify();
 		break;
 	case "movie-this":
-		console.log("Begin movie-this")
-		if (process.argv[3]){
-			var userMovieEntry = new movieEntry(process.argv[3], process.argv[3].replace(/ /g,"-"));
-			request('http://www.omdbapi.com/?apikey=trilogy&s=' + userMovieEntry.formattedName, function(err, response, body){
-	  			if (err) {
-	    			return console.log('Error occurred: ' + err);
-	  			}
-	  			var movies = JSON.parse(body);
-	  			var i = 0;
-	  			movies.Search.some(function(movie){
-	  				i++;
-	  				if (movie.Title === userMovieEntry.name){
-	  					userMovieEntry.setYear(movie.Year);
-	  					userMovieEntry.setID(movie.imdbID);
-						request('http://www.omdbapi.com/?apikey=trilogy&i=' + userMovieEntry.ID, function(err, response, body){
-							if (err) {
-				    			return console.log('Error occurred: ' + err);
-				  			}
-				  			var imdbMovie = JSON.parse(body);
-				  			userMovieEntry.setimdbRating(imdbMovie.imdbRating);
-				  			if (imdbMovie.Ratings[1] != undefined){
-					  			userMovieEntry.setRottenTomatoesRating(imdbMovie.Ratings[1].Value)
-				  			} else {
-				  				userMovieEntry.setRottenTomatoesRating(" No Rating Sorry! ")
-				  			}
-				  			userMovieEntry.setCountry(imdbMovie.Country);
-				  			userMovieEntry.setLanguage(imdbMovie.Language)
-				  			userMovieEntry.setPlot(imdbMovie.Plot)
-				  			userMovieEntry.setActors(imdbMovie.Actors)
-							userMovieEntry.displayMovie();
-						})
-						//Adding a return here to only print one movie
-	  				}
-	  				return i === 1;
-	  			})
-			})
-		} else {
-			var defaultMovieEntry = new movieEntry('Mr. Nobody', 'Mr-Nobody');
-			request('http://www.omdbapi.com/?apikey=trilogy&s=' + defaultMovieEntry.formattedName, function(err, response, body){
-	  			if (err) {
-	    			return console.log('Error occurred: ' + err);
-	  			}
-	  			var movies = JSON.parse(body);
-	  			movies.Search.forEach(function(movie){
-	  				if ((movie.Title === "Mr. Nobody") && (movie.Year === '2009')){
-	  					defaultMovieEntry.setYear(movie.Year);
-	  					defaultMovieEntry.setID(movie.imdbID);
-						request('http://www.omdbapi.com/?apikey=trilogy&i=' + defaultMovieEntry.ID, function(err, response, body){
-							if (err) {
-				    			return console.log('Error occurred: ' + err);
-				  			}
-				  			var imdbMovie = JSON.parse(body);
-				  			defaultMovieEntry.setimdbRating(imdbMovie.imdbRating);
-				  			defaultMovieEntry.setRottenTomatoesRating(imdbMovie.Ratings[1].Value)
-				  			defaultMovieEntry.setCountry(imdbMovie.Country);
-				  			defaultMovieEntry.setLanguage(imdbMovie.Language)
-				  			defaultMovieEntry.setPlot(imdbMovie.Plot)
-				  			defaultMovieEntry.setActors(imdbMovie.Actors)
-							defaultMovieEntry.displayMovie();
-						})
-	  				}
-	  			})
-			})
-		}
+		doMovie();
 		break;
 	case "do-what-it-says":
 		console.log("Begin do-what-it-says")
+		fs.readFile("random.txt", "utf8", function(err, data){
+			var partsOfStr = data.split(',');
+
+			partsOfStr.forEach(function(str){
+				console.log(str)
+			})
+		})
 		break;
 	default:
 		console.log("Whoops!  You need to give me a command!")
